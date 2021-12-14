ab1e1804744b291fbe9e3bcdb3c517b21710bb2e
Jhon Marreros Guzman <j_marreros@hotmail.com>
2021-12-14 10:48:09 -0500

    Agregado validación de tipo de archivo

--- upload-custom-file.php
+++ upload-custom-file.php
@@ -29,23 +29,29 @@ function dcms_upload_file_settings_callback() {
 	<?php
 
 	if(isset($_FILES['upload-file'])) {
-
 		global $wp_filesystem;
 		WP_Filesystem();
 
-		$content_directory = $wp_filesystem->wp_content_dir() . 'uploads/archivos-subidos/';
-		$wp_filesystem->mkdir( $content_directory );
-
-
 		$name_file = $_FILES['upload-file']['name'];
 		$tmp_name = $_FILES['upload-file']['tmp_name'];
+		$allow_extensions = ['xls', 'xlsx', 'csv'];
+
+		// File type validation
+		$path_parts = pathinfo($name_file);
+		$ext = $path_parts['extension'];
+
+		if ( ! in_array($ext, $allow_extensions) ) {
+			echo "Error - File type not allowed";
+			return;
+		}
+
+		$content_directory = $wp_filesystem->wp_content_dir() . 'uploads/archivos-subidos/';
+		$wp_filesystem->mkdir( $content_directory );
 
 		if( move_uploaded_file( $tmp_name, $content_directory . $name_file ) ) {
 			echo "File was successfully uploaded";
 		} else {
 			echo "The file was not uploaded";
 		}
-
 	}
 }
-
